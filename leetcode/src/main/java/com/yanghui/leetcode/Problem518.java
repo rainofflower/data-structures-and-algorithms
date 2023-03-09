49636e183f4fe84580e0f4989efe18e642afc7b4
yanghui <yanghuiindex@gmail.com>
2023-03-09 10:20:02 +0800

    518

--- leetcode/src/main/java/com/yanghui/leetcode/Problem518.java
+++ leetcode/src/main/java/com/yanghui/leetcode/Problem518.java
@@ -10,6 +10,31 @@ public class Problem518 {
         //5
         //dp(0) = 1
         //dp(n) = sum(dp(n-coin)) n >= coin
+
+        /**
+         * dp(5)
+         * 5 = 5
+         * 5 = 2+2+1
+         * 5 = 2+1+1+1
+         * 5 = 1+1+1+1+1
+         * 共4种组合
+         *
+         * dp(4)    -1
+         * 4 = 2+2      +1
+         * 4 = 2+1+1    +1
+         * 4 = 1+1+1+1  +1
+         * 共3种组合
+         *
+         * dp(3)    -2
+         * 3 = 2+1      +2
+         * 3 = 1+1+1    +2
+         * 共2种组合
+         *
+         * dp(0)    -5
+         * 0            +5
+         * 共1种组合
+         *
+         */
         System.out.println(change(5, new int[]{1,2,5}));
     }
 
@@ -18,13 +43,17 @@ public class Problem518 {
         dp[0] = 1;
         for(int coin : coins){
             for(int i = coin; i<=amount; i++){
-                System.out.println("===");
-                System.out.println("coin = "+coin);
-                System.out.println("i = "+i+", coin = "+coin +", dp["+i+"]"+"="+dp[i]);
+                System.out.println("coin = "+coin+", i="+i);
+                System.out.println("1----dp["+i+"]"+"="+dp[i]+", dp["+(i-coin)+"]="+dp[i-coin]);
                 dp[i] += dp[i - coin];
-                System.out.println("i = "+i+", coin = "+coin +", dp["+i+"]"+"="+dp[i]+", dp[i-coin]=dp["+(i-coin)+"]="+dp[i-coin]);
+                System.out.println("2----dp["+i+"]"+"=dp["+i+"]+dp["+i+"-"+coin+"]=dp["+i+"]+dp["+(i-coin)+"]="+dp[i]);
+                System.out.println("===");
             }
         }
         return (int)dp[amount];
+    }
+
+    public int change2(int amount, int[] coins) {
+        return 0;
     }
 }
